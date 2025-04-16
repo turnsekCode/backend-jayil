c86c8c804d173f50fb68622ef7f9dbfb3ec964c0
abraham <abraham.turnsek@gmail.com>
2025-04-16 20:41:52 +0200

    cambio en cors server

--- server.js
+++ server.js
@@ -18,7 +18,23 @@ connectCloudinary()
 
 //middlewares
 app.use(express.json())
-app.use(cors({ origin: "https://www.jayil.es/, https://admin-jayil.vercel.app/", credentials: true }));
+//app.use(cors({ origin: "*", credentials: true }));
+const allowedOrigins = [
+  "https://www.jayil.es",
+  "https://admin-jayil.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 
 
